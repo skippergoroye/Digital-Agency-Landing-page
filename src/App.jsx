3e8da46e32dbfb1def146ca4a848fc62198e2104
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-08-22 03:04:36 -0700

    fix: max-widt added to the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className={`bg-primary w-full overflow-hidden`}>
+      <div className={`bg-primary max-w-[1280px] overflow-hidden`}>
         <div className={`bg-secondary ${styles.paddingX}`}>
           <div className={`${styles.boxWidth}`}>
             <Navbar />
